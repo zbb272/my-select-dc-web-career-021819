ab0feead9cef90f45f393d544701a6bc3f5e8167
Zachary <zbberman@yahoo.com>
2019-02-03 15:42:12 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,9 +1,10 @@
 def my_select(collection)
  i = 0 
- 
+ newCollection = []
  
  while i < collection.length 
-  
+  if (yield collection[i])
+    newCollection << collection[i]
   i += 1
  end 
 end
